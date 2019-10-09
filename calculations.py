c003dc6b71cdb6214ccd5f3e452e2095beff784f
Jorden Quast <jordenquast@gmail.com>
2019-10-09 08:52:58 -0500

    Comments! Other people can now read my code!

--- calculations.py
+++ calculations.py
@@ -1,18 +1,25 @@
 def numunits(carbs, perunit, number, startscale, rnge):
+    # Base food units, no high
     units = round(carbs / perunit, 2)
     print("You should give " + str(units) + " for the food.")
+
+    # Extra units for the high
     if number >= startscale:
         exunits = (number - (startscale - rnge)) / (rnge / .5)
         print("You should give an extra " + str(exunits) + " units for the food.")
+        print("You should give a total of " + str(units + exunits) + " units")
     else:
         print("Your number is great! You need to give no extra units!")
 
+    # Finishing
     input("\nPress enter when finished . . . ")
 
 
 def intro():
     data = {}
+    # Ugly code to keep attempting to get the correct input per meal
     while True:
+        # carbs:unit at breakfast
         try:
             data["Breakfast"] = int(input("For every ___ carbs at breakfast, I give 1 unit." +
                                           "(Answer with a whole number): ").strip())
@@ -20,6 +27,7 @@ def intro():
         except ValueError:
             print("I'm sorry, that is not an accepted input, please try again.\n")
     while True:
+        # carbs:unit at lunch
         try:
             data["Lunch"] = int(input("For every ___ carbs at lunch, I give 1 unit.Answer with a whole number): ")
                                 .strip())
@@ -27,6 +35,7 @@ def intro():
         except ValueError:
             print("I'm sorry, that is not an accepted input, please try again.\n")
     while True:
+        # carbs:unit at dinner
         try:
             data["Dinner"] = int(input("For every ___ carbs at dinner, I give 1 unit." +
                                        "(Answer with a whole number): ").strip())
@@ -34,12 +43,14 @@ def intro():
         except ValueError:
             print("I'm sorry, that is not an accepted input, please try again.\n")
     while True:
+        # Bg for start of sliding scale
         try:
             data["startScale"] = int(input("At what blood glucose does your sliding scale begin: ").strip())
             break
         except ValueError:
             print("I'm sorry, that is not an accepted input, please try again.\n")
     while True:
+        # Range for sliding scale
         try:
             data["rnge"] = int(input("For every ___ points above the start of my sliding scale, I increase my dosage."
                                      "(Answer with a whole number): ").strip())
@@ -50,6 +61,7 @@ def intro():
 
 
 def getcarbs():
+    # Loop simply so its not in the main. Keeps trying to get carbs until number is input
     while True:
         try:
             carbs = int(input("How many carbs are in your meal: ").strip())
@@ -60,6 +72,7 @@ def getcarbs():
 
 
 def getnum():
+    # Loop simply so its not in the main. Keeps trying to get BG until number is input
     while True:
         try:
             number = int(input("What is your current blood glucose: ").strip())
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import json
 import calculations
 
+# Attempting to open json file to get dict, otherwise running setup
 try:
     with open('data.json') as file:
         data = json.load(file)
 
 except FileNotFoundError:
     data = calculations.intro()
+
+# Getting which meal to use
 meal = ''
 while meal != 'b' and meal != 'l' and meal != 'd' and meal != 's':
     meal = (input("Hello, are you treating for (B)reakfast, (L)unch, (D)inner, or are you trying to change your"
                   "(S)ettings? ").strip()).lower()
 
+# Different options based on meal, or doing settings
 if meal != 's':
     carb = calculations.getcarbs()
     number = calculations.getnum()
@@ -26,5 +30,6 @@ elif meal == 's':
     data = calculations.intro()
     input("Thank you, press enter when finished . . . ")
 
+# Dumping data to json file
 with open('data.json', 'w') as file:
     json.dump(data, file)
